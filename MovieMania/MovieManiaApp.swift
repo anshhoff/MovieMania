f09e15dbff24d8ba0052a5b8109d1dac3c6b5033
Anshhoff <anshh2488@gmail.com>
2025-03-27 12:11:26 +0530

    Initial Commit

---
+++ MovieMania/MovieManiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieManiaApp.swift
+//  MovieMania
+//
+//  Created by Ansh Hardaha on 27/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieManiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovieManiaTests/MovieManiaTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieManiaTests.swift
+//  MovieManiaTests
+//
+//  Created by Ansh Hardaha on 27/03/25.
+//
+
+import Testing
+@testable import MovieMania
+
+struct MovieManiaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
